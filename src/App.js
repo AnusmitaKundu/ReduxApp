e5961b72fcd6143e04249b0a96b3c7d1de843c62
“Your <03anusmitakundu@gmail.com>
2024-01-27 14:25:05 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github is complicated!!
         </a>
       </header>
     </div>
