a7d8e452951cc48f1bd5b4d298a49a3e2e04f1b6
“Your <03anusmitakundu@gmail.com>
2024-02-29 10:09:38 +0530

    changed App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          this is a redux App.
+          this is an updated redux App.
         </p>
         <a
           className="App-link"
